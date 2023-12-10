86020cba74209aa8d4e9b1cbe4d12a81939781fd
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2023-12-10 19:16:47 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,24 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>lotowinner 974</title>
         <link rel="icon" href="/favicon.ico" />
+    
       </Head>
 
       <main>
         <iframe
           src="https://gamma.app/embed/wi854kfuq7j7sw2"
-          style="width: 700px; max-width: 100%; height: 450px" 
-          allow="fullscreen"
+          style={{ width: '100%', height: '100vh', border: 'none' }}
+          allowFullScreen
         ></iframe>
       </main>
-
-      <Footer />
     </div>
-  )
-}
+  );
+};
+
 export default HomePage;
