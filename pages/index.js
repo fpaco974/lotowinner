521a611291e5968f9a87c82bdfe12c369d252cea
fpaco974 <136788267+fpaco974@users.noreply.github.com>
2023-12-12 13:56:42 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ const HomePage = () => {
   return (
     <div>
       <Head>
-        <title>lotowinner 974</title>
+        <title>loto-winner</title>
         <link rel="icon" href="/favicon.ico" />
-    
+        <meta name="description" content="lotowinner, la plateforme qui vous rapproche de la victoire, jeu euromillions, pmu, loto." />
+        <meta name="google-site-verification" content="EJhORV8fw4C4ZZ3Kx-bUHRNDMJ7DgiHWe01eFxFe63w" />
       </Head>
 
       <main>
